ff2009c2b807702700924dc018a37abf65997a38
Henry <heyuyang@usc.edu>
2019-11-04 18:34:36 -0800

    Henry 1st commit with only update Profile

---
+++ app/src/main/java/itp341/verduzco/salvador/usclassifieds/UpdateProfile.java
@@ -0,0 +1,14 @@
+package itp341.verduzco.salvador.usclassifieds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}

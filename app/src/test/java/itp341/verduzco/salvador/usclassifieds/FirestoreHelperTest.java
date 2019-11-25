222dd98ec56edd094ad930f973690547402f2361
Basil Chatha <basil.chatha8@gmail.com>
2019-11-24 20:28:53 -0800

    started firestorehelpertest

---
+++ app/src/test/java/itp341/verduzco/salvador/usclassifieds/FirestoreHelperTest.java
@@ -0,0 +1,19 @@
+package itp341.verduzco.salvador.usclassifieds;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class FirestoreHelperTest {
+    public static final String TAG = FirestoreHelper.class.getSimpleName();
+    private FirebaseFirestore firebaseFirestore;
+
+    @Test
+    public void getItemsRefTest() {
+
+    }
+}
